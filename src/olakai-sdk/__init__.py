67fbdf7606dbd1bd44d99d864a44fde7e43657bd
SaschaR0de <sascha@olakai.ai>
2025-07-18 11:06:30 -0700

    export changes

--- src/olakai-sdk/__init__.py
+++ src/olakai-sdk/__init__.py
@@ -10,7 +10,7 @@ from .client import (
     clear_queue,
     flush_queue
 )
-from .monitor import monitor, add_middleware, remove_middleware
+from .monitor import olakai_monitor, add_middleware, remove_middleware
 from .types import SDKConfig, MonitorOptions, Middleware, MonitorPayload
 from .logger import get_default_logger, safe_log
 
@@ -18,7 +18,7 @@ __version__ = "0.1.0"
 
 __all__ = [
     "init_client",
-    "monitor", 
+    "olakai_monitor", 
     "send_to_api",
     "get_config",
     "get_queue_size", 
